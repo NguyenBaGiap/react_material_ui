fac0ae2b3590645e03eb812581f51e9e20bc7582
giapnb2 <nguyenbagiap_t57@hus.edu.vn>
2021-05-26 14:30:30 +0700

    -- test husky

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline/CssBaseline';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-import { Provider } from 'react-redux';
 import App from './App';
 
 

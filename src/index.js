3431cd428cd28a3d448adef45d5f7e640a30dc98
giapnb2 <nguyenbagiap_t57@hus.edu.vn>
2021-05-26 10:58:10 +0700

    --init project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline/CssBaseline';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const globalTheme = createMuiTheme({
+    typography: {
+        useNextVariants: true
+    }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <React.Fragment>
+          <CssBaseline />
+          <MuiThemeProvider theme={globalTheme}>
+              <App />
+          </MuiThemeProvider>
+      </React.Fragment>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
